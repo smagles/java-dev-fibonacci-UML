c78105da117d3caf92e08e490f18ba23375b3b49
smagles <lesialist@icloud.com>
2023-08-20 15:00:25 +0200

    result 10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,13 +9,13 @@ public class Main {
     public static void main(String[] args) {
 
         FibonacciDP fibonacciDP = new FibonacciDP();
-        System.out.println(fibonacciDP.fibonacci(-10));
+        System.out.println(fibonacciDP.fibonacci(10));
 
         FibonacciRecursive fibonacciRecursive = new FibonacciRecursive();
-        System.out.println(fibonacciRecursive.fibonacci(-10));
+        System.out.println(fibonacciRecursive.fibonacci(10));
 
         FibonacciIterative fibonacciIterative = new FibonacciIterative();
-        System.out.println(fibonacciIterative.fibonacci(0));
+        System.out.println(fibonacciIterative.fibonacci(10));
 
     }
 }
